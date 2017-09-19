36f4cf6ce2be266589fc698d98279e1fdf5d2147
xx254 <xx254@cornell.edu>
2017-09-18 21:29:38 -0400

    big data

---
+++ README.md
@@ -0,0 +1,27 @@
+# play-big-data
+
+Play with various big data frameworks
+
+Description
+
+1. Develop a Big Data Platform to analyze real stock data.
+
+2. Implement a high performance data ingestion layer using Apache Kafka (200k msg/s).
+
+3. Develop a data storage layer using Apache Cassandra to store time series data.
+
+4. Implement a data stream processing pipeline using Apache Spark.
+
+5. Develop a visualization web app using Redis, Node.js, and D3.js.
+
+6. Create a scalable cloud deployment environment using Docker, Apache Mesos.
+
+Skills
+
+● Programming languages: Python, JavaScript, Shell Scripting
+
+● Distributed Systems: Apache Kafka, Apache Cassandra, Apache Spark, Apache Mesos
+
+● Web Development: Redis, Nodejs, D3.js, jQuery
+
+● DevOps Infrastructure: Docker, Mesos
---
+++ spark/stream-processing.py
@@ -0,0 +1,99 @@
+import atexit
+import logging
+import json
+import sys
+import time
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaError, KafkaTimeoutError
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+logger_format = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=logger_format)
+logger = logging.getLogger('stream-processing')
+logger.setLevel(logging.INFO)
+
+topic = None
+target_topic = None
+brokers = None
+kafka_producer = None
+
+
+def shutdown_hook(producer):
+    """
+    a shutdown hook to be called before the shutdown
+    :param producer: instance of a kafka producer
+    :return: None
+    """
+    try:
+        logger.info('Flushing pending messages to kafka, timeout is set to 10s')
+        producer.flush(10)
+        logger.info('Finish flushing pending messages to kafka')
+    except KafkaError as kafka_error:
+        logger.warn('Failed to flush pending messages to kafka, caused by: %s', kafka_error.message)
+    finally:
+        try:
+            logger.info('Closing kafka connection')
+            producer.close(10)
+        except Exception as e:
+            logger.warn('Failed to close kafka connection, caused by: %s', e.message)
+
+
+def process(timeobj, rdd):
+
+    # - do average
+    num_of_records = rdd.count()
+    if num_of_records == 0:
+        return
+    
+    # - sum up all the price in this rdd
+    # - for each rdd record, do something (take out the LastTradingPrice, json) -> map
+    # - for all the rdd record, sum up -> reduce
+       
+    price_sum = rdd \
+        .map(lambda record: float(json.loads(record[1].decode('utf-8'))[0].get('LastTradePrice'))) \
+        .reduce(lambda a, b: a + b)
+
+    average = price_sum / num_of_records
+    logger.info('Received %d records from kafka, average price is %f' % (num_of_records, average))
+    current_time = time.time()
+    data = json.dumps({
+        'timestamp': current_time,
+        'average': average
+    })
+    try:
+        kafka_producer.send(target_topic, value=data)
+    except KafkaError as error:
+        logger.warn('Failed to send average stock price to kafka, caused by: %s', error.message)
+
+if __name__ == '__main__':
+
+    # - sys.argv is an array:
+    # - sys.argv[0] stream-process.py, sys.argv[1] broker location, sys.argv[2] topic
+    if len(sys.argv) != 4:
+        print("Usage: stream-process.py [topic] [target-topic] [broker-list]")
+        exit(1)
+
+    # - create SparkContext and StreamingContext
+    sc = SparkContext("local[2]", "StockAveragePrice")
+    sc.setLogLevel('ERROR')
+    ssc = StreamingContext(sc, 5)
+
+    topic, target_topic, brokers = sys.argv[1:]
+
+    # - instantiate a kafka stream for processing
+    directKafkaStream = KafkaUtils.createDirectStream(ssc, [topic], {'metadata.broker.list': brokers})
+    directKafkaStream.foreachRDD(process)
+
+    # - instantiate a simple kafka producer
+    kafka_producer = KafkaProducer(
+        bootstrap_servers=brokers
+    )
+
+    # - setup proper shutdown hook
+    atexit.register(shutdown_hook, kafka_producer)
+
+    ssc.start()
+    ssc.awaitTermination()
